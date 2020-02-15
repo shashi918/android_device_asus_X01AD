5326b4a06ef6aefd1eaf6157ec96d4d950e5bfeb
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-14 22:39:33 -0500

    X01AD: Build custom audio amplifier
    Change-Id: I369555a9fcb06065177636b436d2e006c7a0b07d

--- device.mk
+++ device.mk
@@ -48,6 +48,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
+    audio_amplifier.msm8953 \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.primary.msm8953 \
