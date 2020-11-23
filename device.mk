06d3e544305349bc93559e5066a04f9ebdbf8381
Michael Bestas <mkbestas@lineageos.org>
2020-11-23 20:47:24 +0200

    X01AD: Don't build textclassifier
    * Target no longer exists
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f

--- device.mk
+++ device.mk
@@ -410,10 +410,6 @@ PRODUCT_BOOT_JARS += \
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
-    
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
