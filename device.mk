6c31a4ef325f95687059d177729f5900e3bf36b7
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-01-26 15:23:21 +0700

    X01AD: Update blob Bluetooth from daisy-user-10-QKQ1.191002.002-V11.0.10.0.QDLMIXM-release-keys

--- device.mk
+++ device.mk
@@ -56,13 +56,6 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     tinymix
 
-PRODUCT_PACKAGES += \
-    libhdmiedid \
-    libhfp \
-    libsndmonitor \
-    libspkrprot \
-    libssrec
-
 # Audio Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
@@ -104,6 +97,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 PRODUCT_PACKAGES += \
