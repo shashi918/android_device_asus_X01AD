c4f81181231928c6da3b462c5ac36b39c9e9660d
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-01-26 09:49:21 +0700

    X01AD: Build libgui_vendor from source

--- device.mk
+++ device.mk
@@ -117,6 +117,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
+    libgui_vendor \
     libstdc++.vendor
 
 PRODUCT_PACKAGES += \
