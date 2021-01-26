b7c18fc405572684f8d463112ad74350430f09d6
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-01-26 09:43:53 +0700

    X01AD: update camera prop

--- device.mk
+++ device.mk
@@ -117,7 +117,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor
 
 PRODUCT_PACKAGES += \
