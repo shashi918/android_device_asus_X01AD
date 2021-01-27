47758ecf3da53b38164f481e5b4e805aa809dbd4
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-01-27 14:50:56 +0700

    X01AD: Update hals camera

--- device.mk
+++ device.mk
@@ -105,11 +105,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
     libgui_vendor \
     libstdc++.vendor
