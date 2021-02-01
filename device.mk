0f9318796c4e3b9f3cc2c307a9aea09a79ffbdf0
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-02-01 11:37:50 +0700

    Revert "X01AD: Drop snap overlay"
    This reverts commit 5b920da937646a44d4bcebdd3370721d10023f5a.

--- device.mk
+++ device.mk
@@ -13,7 +13,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1520
