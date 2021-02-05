e17c8ec79075cb98d6886c17ef83d19c4b6b11ac
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-02-05 16:48:06 +0700

    X01AD: Camera default work ?

--- device.mk
+++ device.mk
@@ -101,13 +101,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
+    libmm-qcamera \
     libstdc++.vendor
 
 PRODUCT_PACKAGES += \
